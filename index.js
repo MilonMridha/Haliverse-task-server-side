1676c704b52a8951678799b8a0d2bc87ea32b94e
MilonMridha <milonmridha570@gmail.com>
2022-07-05 04:54:59 +0600

    heliverse job task

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000
+// const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+// middle wares ------------------- 
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t7ino.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log("db connected");
+
+
+
+app.get('/', (req, res) => {
+    res.send('Heliverse task is running')
+})
+
+app.listen(port, () => {
+    console.log(`Heliverse listening on port ${port}`)
+})
